161ef0ec9d42a5cfb400c1a9be7a20799a5facd6
Alan Ghelardi <alan.ghelardi@gmail.com>
2015-09-19 19:32:21 -0300

    Preparando para o deploy no Heroku. Criados novos arquivos de environment. Alterado o procfile para exportar a variável de sistema que identifica o ambiente de produção.

--- src/main/java/com/meujornal/models/usuarios/Usuario.java
+++ src/main/java/com/meujornal/models/usuarios/Usuario.java
@@ -47,7 +47,7 @@ public class Usuario implements Serializable {
 	private String email;
 	@NotNull
 	@Convert(converter = RoleConverter.class)
-	private Role papel = Role.ADMINISTRATOR;
+	private Role papel = Role.USER;
 	@NotBlank
 	private String perguntaDeSeguranca;
 	@NotBlank
