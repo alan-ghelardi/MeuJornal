98234171cf0c593e22253a5668cd4f24aa0f5022
Alan Ghelardi <alan.ghelardi@gmail.com>
2015-10-20 21:52:40 -0200

    Adicionando suporte aà conexão com banco de dados no Openshift.

--- src/main/java/com/meujornal/infrastructure/persistence/settings/DatabaseSettings.java
+++ src/main/java/com/meujornal/infrastructure/persistence/settings/DatabaseSettings.java
@@ -5,7 +5,8 @@ import javax.enterprise.context.ApplicationScoped;
 /**
  * Provê as configurações do banco de dados. Internamente utiliza o pattern
  * state para alternar entre as configurações de desenvolvimento e de produção
- * dependendo da presença ou não da variável de sistema DATABASE_URL.
+ * dependendo da presença ou não da variável de sistema que contém a URL do
+ * banco de dados de produção.
  * 
  * @author Alan Ghelardi
  *
@@ -13,21 +14,20 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class DatabaseSettings {
 
-	private static final String ENVIRONMENT_VARIABLE = "DATABASE_URL";
+	private static final String DATABASE_URL = "OPENSHIFT_POSTGRESQL_DB_URL";
 
 	private final EnvironmentSettings settings;
 
 	public DatabaseSettings() {
 		if (isInDeployEnvironment()) {
-			settings = new DeploymentSettings(
-					System.getenv(ENVIRONMENT_VARIABLE));
+			settings = new DeploymentSettings(System.getenv(DATABASE_URL));
 		} else {
 			settings = new DevelopmentSettings();
 		}
 	}
 
 	private boolean isInDeployEnvironment() {
-		return System.getenv(ENVIRONMENT_VARIABLE) != null;
+		return System.getenv(DATABASE_URL) != null;
 	}
 
 	public String getUsername() {
